7644543b3752b57632e0594a60610856beedab85
Octavio Vaca <octavio_1982@yahoo.com.mx>
2017-08-10 16:33:47 -0500

    primer practica de java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	//public static void main(String ... args) {	
+      System.out.println(args[0]);
+      System.out.println(args[1]);
+      System.out.println(args[2]);
+	  	
+	}
+
+}
